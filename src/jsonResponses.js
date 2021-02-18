1b56713a300d974477a02c8db960627645e6f567
NicholasPaniccioli <njp7745@g.rit.edu>
2021-02-18 01:18:45 -0500

    almost done with basics shown in video. Current error, uknown cause

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,32 +1,43 @@
 const respondJSON = (request, response, status, object) => {
-    response.writeHead(status, {'Content-Type': 'application/json'});
-    response.write(JSON.stringify(object));
-    response.end();
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
 };
 
 const success = (request, response) => {
-    const responseJSON = {
-        message: 'This is a successful response',
-    };
+  const responseJSON = {
+    message: 'This is a successful response',
+  };
 
-    respondJSON (request, response, 200, responseJSON);
+  respondJSON(request, response, 200, responseJSON);
 };
 
 const badRequest = (request, response, params) => {
-    const responseJSON = {
-        message: 'this request has the require parameteres',
-    };
+  const responseJSON = {
+    message: 'this request has the require parameters',
+  };
+
+  if (!params.valid || params.valid !== 'true') {
+    responseJSON.message = 'Missing valid query parameter set equal to true';
+    responseJSON.id = 'badRequest';
+
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  return respondJSON(request, response, 200, responseJSON);
 };
 
 const notFound = (request, response) => {
-    const responseJSON = {
-        message: 'the page you are looking for was not found.',
-        id: 'notFound',
-    };
+  const responseJSON = {
+    message: 'the page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  return respondJSON(request, response, 400, responseJSON);
 };
 
 module.exports = {
-    success,
-    badRequest, 
-    notFound,
-}
+  success,
+  badRequest,
+  notFound,
+};
--- src/server.js
+++ src/server.js
@@ -1,25 +1,29 @@
 const http = require('http');
 const url = require('url');
+const query = require('querystring');
 const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-  GET: {
-    '/': htmlHandler.getIndex,
-    '/style.css': htmlHandler.getStyle,
-    index: htmlHandler.getIndex,
-  },
+  '/': htmlHandler.getIndex,
+  success: jsonHandler.success,
+  badRequest: jsonHandler.badRequest,
+  notFound: jsonHandler.notFound,
+  '/style.css': htmlHandler.getStyle,
 };
 
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
-  const acceptedTypes = request.headers.accept.split(',');
+  const params = query.parse(parsedUrl.query);
+  // const acceptedTypes = request.headers.accept.split(',');
 
   if (urlStruct[parsedUrl.pathname]) {
-    urlStruct[parsedUrl.pathname](request, response, acceptedTypes);
+    // urlStruct[parsedUrl.pathname](request, response, acceptedTypes);
+    urlStruct[parsedUrl.pathname](request, response, params);
   } else {
-    urlStruct.index(request, response);
+    urlStruct.notFound(request, response, params);
   }
 };
 
